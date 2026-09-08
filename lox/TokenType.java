0c3a9b7f0fd2d3a7dfe85bc0099fa7bebe7cbd1b
luis-antoniosi <luisgustavo_antoniosi@hotmail.com>
2026-09-08 14:09:30 -0300

    first commit; chapter Scanning done

---
+++ lox/TokenType.java
@@ -0,0 +1,22 @@
+package lox;
+
+enum TokenType {
+    // single-character
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // one or two characters
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUNCTION, FOR, IF, NULL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
